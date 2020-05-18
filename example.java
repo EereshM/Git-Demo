987c7115833ff0ac4e62e6bc8ac7ba352a1b3afc
EereshM <eereshms123@gmail.com>
2020-05-18 15:38:30 +0530

    added multilpe SOPs into example file

--- example.java
+++ example.java
@@ -3,5 +3,18 @@ public class Example {
 public static void main(String[] args){
 
 System.out.println("Hello world!! Exanple Demo");
+System.out.println("Hello world!! Exanple Demo");
+System.out.println("Hello world!! Exanple Demo");
+
+System.out.println("Hello world!! Exanple Demo");
+
+
+System.out.println("Hello world!! Exanple Demo");
+
+
+System.out.println("Hello world!! Exanple Demo");
+
+System.out.println("Hello world!! Exanple Demo");
+System.out.println("Hello world!! Exanple Demo");
 	}
 }
